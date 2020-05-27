320eb9afbf53b515227656c83d8fbaad11c2942a
Steven Woodward <steveanthony999@gmail.com>
2020-05-26 19:53:09 -0700

    connected redux to logs component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -15,6 +15,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 // };
 
 // the above, refactored
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
